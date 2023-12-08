0bb7fe4b009fd05614c68b6613e91b913334a60d
steinerjakob <jakob.steiner95@gmail.com>
2023-12-08 11:51:11 +0100

    feat: add first leaflet map

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,39 @@
 <template lang="pug">
 v-container.fill-height
-	v-responsive.align-center.text-center.fill-height
-		v-img(height='300', src='@/assets/logo.svg')
-			.text-body-2.font-weight-light.mb-n1 Welcome to
-			h1.text-h2.font-weight-bold Vuetify
-			.py-14
-				v-row.d-flex.align-center.justify-center
-					v-col(cols='auto')
-						v-btn(
-							href='https://vuetifyjs.com/components/all/',
-							min-width='164',
-							rel='noopener noreferrer',
-							target='_blank',
-							variant='text'
-						)
-							v-icon(icon='mdi-view-dashboard', size='large', start='')
-								| Components
-					v-col(cols='auto')
-						v-btn(
-							color='primary',
-							href='https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
-							min-width='228',
-							rel='noopener noreferrer',
-							size='x-large',
-							target='_blank',
-							variant='flat'
-						)
-							v-icon(icon='mdi-speedometer', size='large', start='')
-								| Get Started
-					v-col(cols='auto')
-						v-btn(
-							href='https://community.vuetifyjs.com/',
-							min-width='164',
-							rel='noopener noreferrer',
-							target='_blank',
-							variant='text'
-						)
-							v-icon(icon='mdi-account-group', size='large', start='')
-								| Community
+	#map.h-100.w-100
 </template>
 
 <script lang="ts" setup>
-//
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+	const map = L.map('map').fitWorld();
+
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '© OpenStreetMap'
+	}).addTo(map);
+
+	map.locate({ setView: true, maxZoom: 16 });
+
+	function onLocationFound(e: any) {
+		const radius = e.accuracy;
+
+		L.marker(e.latlng)
+			.addTo(map)
+			.bindPopup('You are within ' + radius + ' meters from this point')
+			.openPopup();
+
+		L.circle(e.latlng, radius).addTo(map);
+	}
+
+	map.on('locationfound', onLocationFound);
+	function onLocationError(e: any) {
+		alert(e.message);
+	}
+
+	map.on('locationerror', onLocationError);
+});
 </script>
