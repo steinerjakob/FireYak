4ae8c5ee3031fecafdfc129538f091696fe79785
steinerjakob <jakob.steiner95@gmail.com>
2023-12-08 12:03:41 +0100

    refactor: disable vite pwa for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,46 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import ViteFonts from 'unplugin-fonts/vite';
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-import {VitePWA} from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    vuetify({
-      autoImport: true,
-      styles: {
-        configFile: 'src/styles/settings.scss',
-      },
-    }),
-    ViteFonts({
-      google: {
-        families: [
-          {
-            name: 'Roboto',
-            styles: 'wght@100;300;400;500;700;900',
-          },
-        ],
-      },
-    }),
-    VitePWA({ registerType: 'autoUpdate' }),
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
-  },
-  server: {
-    port: 3000,
-  },
-})
+	plugins: [
+		vue({
+			template: { transformAssetUrls }
+		}),
+		// https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+		vuetify({
+			autoImport: true,
+			styles: {
+				configFile: 'src/styles/settings.scss'
+			}
+		}),
+		ViteFonts({
+			google: {
+				families: [
+					{
+						name: 'Roboto',
+						styles: 'wght@100;300;400;500;700;900'
+					}
+				]
+			}
+		}) //,
+		// VitePWA({ registerType: 'autoUpdate' })
+	],
+	define: { 'process.env': {} },
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		},
+		extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
+	},
+	server: {
+		port: 3000
+	}
+});
