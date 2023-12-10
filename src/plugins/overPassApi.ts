a662cc3591b373d2f789dda990a17e0f653b42f7
steinerjakob <jakob.steiner95@gmail.com>
2023-12-10 16:03:41 +0100

    feat: add firestations to the map

--- src/plugins/overPassApi.ts
+++ src/plugins/overPassApi.ts
@@ -1,6 +1,6 @@
 import L, { LatLngBounds } from 'leaflet';
 
-// import iconFirestation from '../assets/markers/firestation.png';
+import iconFirestation from '../assets/markers/firestation.png';
 import iconHydrant from '../assets/markers/hydrant.png';
 import iconPump from '../assets/markers/pump.png';
 import iconUnderground from '../assets/markers/underground.png';
@@ -12,9 +12,18 @@ const OverpassBaseURL = `https://overpass-api.de/api/interpreter?data=`;
 
 interface OverPassElement {
 	id: number;
-	lat: number;
-	lon: number;
-	tags: { [key: string]: string };
+	type: string;
+	lat?: number;
+	lon?: number;
+	center?: {
+		lat: number;
+		lon: number;
+	};
+	tags: {
+		// emergency: 'fire_hydrant' | 'water_tank' | 'suction_point' | 'fire_water_pond' | undefined;
+		// 'firehydrant:type': 'pillar' | 'underground' | 'wall' | undefined;
+		[key: string]: string;
+	};
 }
 export async function fetchMarkerData(mapBounds: LatLngBounds): Promise<OverPassElement[]> {
 	let fetchData = '[out:json][timeout:15];(';
@@ -48,27 +57,35 @@ export async function fetchMarkerData(mapBounds: LatLngBounds): Promise<OverPass
 	return data?.elements as OverPassElement[];
 }
 
-function getIconForNode(emergency: string, type: string): L.Icon {
+function getIconForNode(element: OverPassElement): L.Icon {
 	let iconData = iconHydrant;
-	switch (emergency) {
-		case 'fire_hydrant':
-			iconData =
-				{
-					pillar: iconHydrant,
-					underground: iconUnderground,
-					wall: iconWall
-				}[type] || iconHydrant;
-			break;
-		case 'suction_point':
-			iconData = iconPump;
-			break;
-		case 'fire_water_pond':
-			iconData = iconWater;
-			break;
-		case 'water_tank':
-			iconData = iconWaterTank;
-			break;
+	if (element.type === 'node') {
+		const emergency = element.tags?.emergency;
+		const type = element.tags?.['firehydrant:type'];
+		switch (emergency) {
+			case 'fire_hydrant':
+				iconData =
+					{
+						pillar: iconHydrant,
+						underground: iconUnderground,
+						wall: iconWall
+					}[type] || iconHydrant;
+				break;
+			case 'suction_point':
+				iconData = iconPump;
+				break;
+			case 'fire_water_pond':
+				iconData = iconWater;
+				break;
+			case 'water_tank':
+				iconData = iconWaterTank;
+				break;
+		}
 	}
+	if (element.type === 'way') {
+		iconData = iconFirestation;
+	}
+
 	return L.icon({
 		iconUrl: iconData,
 		iconSize: [32, 32]
@@ -79,16 +96,14 @@ export async function getMarkersForView(mapBounds: LatLngBounds) {
 	try {
 		const mapElements = await fetchMarkerData(mapBounds);
 		for (const element of mapElements) {
-			if (element.lat && element.lon) {
-				const latLng = L.latLng(element.lat, element.lon);
-				// todo add additional marker information
-				// icon and so on..
-				const markerIcon = getIconForNode(
-					element.tags?.emergency,
-					element.tags?.['firehydrant:type']
-				);
-				markerList.push(L.marker(latLng, { icon: markerIcon }));
-			}
+			const latLng = L.latLng(
+				(element?.lat || element.center?.lat) as number,
+				(element.lon || element.center?.lon) as number
+			);
+			// todo add additional marker information
+			// icon and so on..
+			const markerIcon = getIconForNode(element);
+			markerList.push(L.marker(latLng, { icon: markerIcon }));
 		}
 	} catch (e) {
 		// ignore error for now
