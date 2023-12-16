ea54172d38fd320216dab575981998a5414b8eea
steinerjakob <jakob.steiner95@gmail.com>
2023-12-16 14:57:39 +0100

    feat: basic PWA support with reload prompt
    Refs: #8

--- src/layouts/default/ViewLayout.vue
+++ src/layouts/default/ViewLayout.vue
@@ -1,9 +1,10 @@
 <template>
 	<v-main>
 		<router-view />
+		<ReloadPrompt></ReloadPrompt>
 	</v-main>
 </template>
 
 <script lang="ts" setup>
-//
+import ReloadPrompt from '@/components/ReloadPrompt.vue';
 </script>
