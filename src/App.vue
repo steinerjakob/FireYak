f1d1c9c992fd5360d2ae173d32cb18787cb22708
Jakob Steiner <jakob.steiner@microtronics.com>
2025-10-15 07:36:57 +0200

    refactor: remove edge-to-edge plugin and update App.vue with UpdateToast component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 	<ion-app>
 		<ion-router-outlet />
+		<UpdateToast></UpdateToast>
 	</ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import UpdateToast from '@/components/UpdateToast.vue';
 </script>
