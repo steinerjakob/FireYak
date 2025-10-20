90e9f8622655bbfb735f75906faa7e4991e49fd4
steinerjakob <jakob.steiner95@gmail.com>
2025-10-20 10:45:24 +0200

    feat: enhance pump calculation logic to update polyline on point changes

--- src/composable/pumpCalculation.ts
+++ src/composable/pumpCalculation.ts
@@ -6,37 +6,52 @@ const layer = new L.LayerGroup();
 let rootMap: L.Map | null = null;
 const suctionPoint: L.Marker = new L.Marker();
 const targetPoint: L.Marker = new L.Marker();
-// eslint-disable-next-line sonarjs/no-unused-collection
 const wayPoints: L.Marker[] = [];
+const line = new L.Polyline([]);
 
 function setMap(map: L.Map) {
 	rootMap = map;
 	layer.addTo(rootMap);
 }
 
+console.log(suctionPoint.getLatLng());
+
+const updatePolyline = () => {
+	if (!suctionPoint.getLatLng() || !targetPoint.getLatLng()) {
+		return;
+	}
+	const allPoints = [suctionPoint, ...wayPoints, targetPoint];
+	const latLngs = allPoints.map((point) => point.getLatLng());
+	line.setLatLngs(latLngs);
+	layer.addLayer(line);
+};
+
+const setSuctionPoint = () => {
+	const latLng = rootMap?.getCenter();
+	suctionPoint.setLatLng(latLng);
+	layer.addLayer(suctionPoint);
+	updatePolyline();
+};
+
+const setTargetPoint = () => {
+	const latLng = rootMap?.getCenter();
+	targetPoint.setLatLng(latLng);
+	layer.addLayer(targetPoint);
+	updatePolyline();
+};
+
+const setWayPoint = () => {
+	const latLng = rootMap?.getCenter();
+	const wayPoint = new L.Marker(latLng);
+	wayPoints.push(wayPoint);
+	layer.addLayer(wayPoint);
+	updatePolyline();
+};
+
 export function usePumpCalculation() {
 	const route = useRoute();
 
 	const isActive = computed(() => route.path.includes('supplyPipe'));
-
-	const setSuctionPoint = () => {
-		const latLng = rootMap?.getCenter();
-		suctionPoint.setLatLng(latLng);
-		layer.addLayer(suctionPoint);
-	};
-
-	const setTargetPoint = () => {
-		const latLng = rootMap?.getCenter();
-		targetPoint.setLatLng(latLng);
-		layer.addLayer(targetPoint);
-	};
-
-	const setWayPoint = () => {
-		const latLng = rootMap?.getCenter();
-		const wayPoint = new L.Marker(latLng);
-		wayPoints.push(wayPoint);
-		layer.addLayer(wayPoint);
-	};
 
 	return { isActive, layer, setMap, setSuctionPoint, setTargetPoint, setWayPoint };
 }
