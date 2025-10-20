709e04c1e68621475f53993de940077d890f3fd1
steinerjakob <jakob.steiner95@gmail.com>
2025-10-20 09:18:07 +0200

    feat: add Supply Pipe Calculation component and update MainMap behavior

---
+++ src/components/SupplyPipeCalculation.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+import { computed, ref, watch } from 'vue';
+import { IonContent, IonCard, IonModal } from '@ionic/vue';
+import MarkerInfo from '@/components/MarkerInfo.vue';
+import MarkerInfoHeader from '@/components/MarkerInfoHeader.vue';
+import { useScreenOrientation } from '@/composable/screenOrientation';
+
+const router = useRouter();
+const route = useRoute();
+const modal = ref();
+const screenOrientation = useScreenOrientation();
+
+const showSupplyPipe = computed(() => route.path.includes('supplyPipe'));
+
+const isMobile = ref(window.innerWidth < 768);
+
+watch(screenOrientation.orientation, () => {
+	isMobile.value = window.innerWidth < 768;
+});
+</script>
+
+<template>
+	<template v-if="isMobile">
+		<ion-modal
+			ref="modal"
+			:is-open="showSupplyPipe"
+			:breakpoints="[0.25, 0.4, 0.5, 0.75]"
+			:initial-breakpoint="0.4"
+			:backdrop-dismiss="false"
+			:backdrop-opacity="0"
+			:showBackdrop="false"
+			:expand-to-scroll="false"
+			handle-behavior="cycle"
+			class="supply-pipe"
+		>
+			<ion-content> </ion-content>
+		</ion-modal>
+	</template>
+	<ion-card v-else-if="showSupplyPipe" class="desktop-card"> </ion-card>
+</template>
+
+<style scoped>
+.supply-pipe {
+	--height: 100%;
+	--width: 100%;
+	--backdrop-opacity: 0;
+}
+
+.desktop-card {
+	position: fixed;
+	left: 0;
+	top: 0;
+	bottom: 0;
+	width: 400px;
+	z-index: 1000;
+
+	border-radius: 0;
+	overflow-y: auto;
+}
+
+@media (max-width: 767px) {
+	.supply-pipe::part(content) {
+		border-radius: 8px 8px 0 0;
+		box-shadow: 0 -2px 6px rgba(0, 0, 0, 0.1);
+	}
+}
+</style>
