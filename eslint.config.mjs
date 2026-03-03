69286f4794d735dec3ea021459a8d86c62dd143e
steinerjakob <jakob.steiner95@gmail.com>
2026-03-03 19:18:45 +0100

    chore: update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,58 +1,50 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import vue from "eslint-plugin-vue";
-import sonarjs from "eslint-plugin-sonarjs";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import vue from 'eslint-plugin-vue';
+import sonarjs from 'eslint-plugin-sonarjs';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+export default defineConfig([
+	globalIgnores([
+		'node_modules/',
+		'package.json',
+		'package-lock.json',
+		'**/index.html',
+		'src/plugins/leaflet.restoreview.js',
+		'src/vite-env.d.ts'
+	]),
+	{
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+			vue,
+			sonarjs,
+			prettier
+		},
 
-export default defineConfig([globalIgnores([
-    "node_modules/",
-    "package.json",
-    "package-lock.json",
-    "**/index.html",
-    "src/plugins/leaflet.restoreview.js",
-    "src/vite-env.d.ts",
-]), {
+		languageOptions: {
+			globals: {
+				...globals.browser
+			},
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        vue,
-        sonarjs,
-        prettier,
-    },
+			ecmaVersion: 'latest',
+			sourceType: 'module',
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		},
 
-        ecmaVersion: "latest",
-        sourceType: "module",
-
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
-        },
-    },
-
-    rules: {
-        "prettier/prettier": "off",
-
-        "@typescript-eslint/ban-ts-comment": ["error", {
-            "ts-ignore": "allow-with-description",
-        }],
-
-        "vue/no-deprecated-slot-attribute": "off",
-    },
-}]);
+		rules: {
+			'prettier/prettier': 'off',
+			'@typescript-eslint/ban-ts-comment': [
+				'error',
+				{
+					'ts-ignore': 'allow-with-description'
+				}
+			],
+			'vue/no-deprecated-slot-attribute': 'off',
+			'no-unused-vars': 'error'
+		}
+	}
+]);
